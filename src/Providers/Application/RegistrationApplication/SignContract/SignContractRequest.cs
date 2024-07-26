452ae9321ccedf4781b77d4e4ed3516a5d5af2ec
antonio.sanchez <antonio.sanchez@leanmind.es>
2024-07-26 02:23:02 +0200

    created example of how not modify an aggregate

---
+++ src/Providers/Application/RegistrationApplication/SignContract/SignContractRequest.cs
@@ -0,0 +1,41 @@
+﻿using LanguageExt;
+using Providers.Business.RegistrationApplication.Contacts.ValueObjects;
+using Providers.Business.RegistrationApplication.Contracts.ValueObjects;
+using Providers.Business.RegistrationApplication.ValueObjects;
+using ValueObjects;
+
+namespace Providers.Application.RegistrationApplication.SignContract
+{
+    public sealed class SignContractRequest
+    {
+        public readonly RegistrationApplicationId ProviderRegistrationApplicationId;
+        public readonly ContractId ContractId;
+
+        public static Validation<ValidationError, SignContractRequest> Create(RequestDto req)
+        {
+            //validations and so on
+            throw new NotImplementedException();
+        }
+
+        private SignContractRequest(
+            RegistrationApplicationId providerRegistrationApplicationId,
+            ContractId contractId)
+        {
+            ProviderRegistrationApplicationId = providerRegistrationApplicationId;
+            ContractId = contractId;
+        }
+
+        public sealed class RequestDto
+        {
+            public readonly Guid ProviderRegistrationApplicationId;
+            public readonly Guid ContractId;
+            public RequestDto(
+                Guid providerRegistrationApplicationId,
+                Guid contractId)
+            {
+                ProviderRegistrationApplicationId = providerRegistrationApplicationId;
+                ContractId = contractId;
+            }
+        }
+    }
+}
---
+++ src/Providers/Application/RegistrationApplication/SignContract/TheWrongWay/SignContractService.cs
@@ -0,0 +1,25 @@
+﻿namespace Providers.Application.RegistrationApplication.SignContract.TheWrongWay
+{
+    public sealed class SignContractService
+    {
+        //private readonly RegistrationApplicationRepository registrationApplicationRepository;
+        //private readonly ContractsRepository contractsRepository;
+
+        void Sign(SignContractRequest request)
+        {
+            //auto contract = contractsRepository.SearchById(request.ContractId);
+            //auto signedContract = contract.Sign();
+            //contractsRepository.Update(signedContract)
+        }
+
+        void SignEvenWorst(SignContractRequest request)
+        {
+            //auto application = registrationApplicationRepository.SearchById(request.ProviderRegistrationApplicationId);
+            //if(application.Contract.IsAlreadySigned()) return error
+
+            //auto contract = contractsRepository.SearchById(request.ContractId);
+            //auto signedContract = contract.Sign();
+            //contractsRepository.Update(signedContract)
+        }
+    }
+}
